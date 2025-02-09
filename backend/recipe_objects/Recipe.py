fd13b38010c13b2d2d18c8d4c04e5ac648ae19ec
Calvin Chen <clc162@duke.edu>
2025-02-08 23:41:30 -0500

    rename ingredientMap to ingredient_dictionary to reflect Python syntax

--- backend/recipe_objects/Recipe.py
+++ backend/recipe_objects/Recipe.py
@@ -1,9 +1,9 @@
 class Recipe:
-  def __init__(self, name, steps, ingredient_map):
+  def __init__(self, name, steps, ingredient_dictionary):
       self.name = name
       self.steps = steps
-      self.ingredientMap = ingredient_map
+      self.ingredient_dictionary = ingredient_dictionary
 
   def get_ingredients_with_quantity(self):
     return [ingredient.report_with_quantity(quantity)
-            for ingredient, quantity in self.ingredientMap.items()]
+            for ingredient, quantity in self.ingredient_dictionary.items()]
