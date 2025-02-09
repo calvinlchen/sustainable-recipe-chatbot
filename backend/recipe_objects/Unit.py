d5441066e3805b3c523f64b44f000e0f58432f56
Johan Nino Espino <johan.ninoespino@duke.edu>
2025-02-08 22:30:43 -0500

    Have to push this class

---
+++ backend/recipe_objects/Unit.py
@@ -0,0 +1,7 @@
+class Unit:
+
+
+    VALID_UNITS = {"teaspoon", "tablespoon", "fluid ounce", "cup", "pint", "quart",
+                   "gallon", "gram", "ounce", "pound", "piece", "slice"}
+    
+    
